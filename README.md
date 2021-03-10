1cfe49261a80f6d168f84f04d764c7be9bc421be
HwangSeungyeon <syhwang.web@gmail.com>
2021-03-10 13:51:37 +0900

    #

---
+++ README.md
@@ -0,0 +1,19 @@
+# restapi-crud-test
+> **RESTapi Test with Kotlin and Spring Boot**
+
+##  機能一覧
+
+1. RESTapi : response, request
+    - GET
+    - POST
+    - DELETE
+    - PUT  
+      
+
+2. RESTapi : response
+    - html
+    - object
+
+## その他
+- Validation(Bean Validation)
+    - https://beanvalidation.org/2.0-jsr380/spec/
--- src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/delete/DeleteApiController.kt
@@ -1,9 +1,14 @@
 package com.example.mvc.controller.delete
 
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/api")
+@Validated //_age는 Bean이 아니기 때문에 어노테이션 필요
 class DeleteApiController {
 
     // 가질수 있는 것
@@ -14,6 +19,11 @@ class DeleteApiController {
     fun deleteMapping(
         // 이름 지정 가능
         @RequestParam(value = "name") _name: String,
+
+        //Validation
+        @NotNull(message = "age 값이 누락되었습니다.")
+        @Min(20, message = "20보다 커야 합니다.")
+
         @RequestParam(name = "age") _age: Int
     ): String {
         println(_name)
@@ -22,7 +32,17 @@ class DeleteApiController {
     }
 
     @DeleteMapping(path = ["/delete-mapping/name/{name}/age/{age}"])
-    fun deleteMappingPath(@PathVariable(value = "name") _name: String, @PathVariable(name = "age") _age: Int): String {
+    fun deleteMappingPath(
+        @PathVariable(value = "name")
+        @Size(min = 2, max = 5)
+        @NotNull
+        _name: String, // aa ~ aaaaa
+
+        //Validation
+        @NotNull(message = "age 값이 누락되었습니다.")
+        @Min(20, message = "20보다 커야 합니다.")
+        @PathVariable(name = "age") _age: Int
+    ): String {
         println(_name)
         println(_age)
         return _name + " " + _age
